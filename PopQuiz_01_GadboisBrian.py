e7c8244b9a4c8eee9f9105ada49b747397151a29
gadboisb5877 <31962124+gadboisb5877@users.noreply.github.com>
2017-09-28 10:07:07 -0400

    Add files via upload

---
+++ PopQuiz_01_GadboisBrian.py
@@ -0,0 +1,15 @@
+# CTI 110
+# Pop Quiz 01
+# Brian Gadbois
+# 9/26/2017
+# Program made to find out averages
+
+firstNum = float (input ('What is your first number:'))
+
+secondNum = float (input ('What is your second number:'))
+
+avg = (firstNum + secondNum) / 2
+
+print ('Your average will be:', avg) 
+
+
